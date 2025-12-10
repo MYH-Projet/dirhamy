74fa9e9db2fda22e77d243dd160335d1135eab54
akram <168238434+akramelassri@users.noreply.github.com>
2025-12-10 19:36:18 +0100

    Implement add transaction ui

--- frontend/transactions-script.js
+++ frontend/transactions-script.js
@@ -1,5 +1,9 @@
 const API_URL = 'http://localhost:3000/api';
 
+const dateInput = document.querySelector('#transaction-form-date');
+
+dateInput.value = new Date().toISOString().split('T')[0]; /* look for a better way */
+
 function getTransactions() {
 
 }
