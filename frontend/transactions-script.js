0013f89914dd89668cfc236dba63c76195c63b29
akram <168238434+akramelassri@users.noreply.github.com>
2025-12-12 19:07:51 +0100

    Finish transaction ui
    Added the final javascript check to add and remove the transfer
    unit when the type chosen is transfer

--- frontend/transactions-script.js
+++ frontend/transactions-script.js
@@ -4,6 +4,18 @@ const dateInput = document.querySelector('#transaction-form-date');
 
 dateInput.value = new Date().toISOString().split('T')[0]; /* look for a better way */
 
+const typeTransactionInput = document.querySelector('#transaction-form-type');
+
+const formTransferUnit = document.querySelector('#form-unit-transfer');
+
+typeTransactionInput.addEventListener('change', e => {
+    if(typeTransactionInput.value === 'transfer') {
+        formTransferUnit.classList.remove('invisible-unit');
+    } else {
+        formTransferUnit.classList.add('invisible-unit');
+    }
+});
+
 function getTransactions() {
 
 }
