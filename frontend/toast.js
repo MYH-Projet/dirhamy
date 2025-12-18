1d2c26f8e5f3e9b66cd8b5198d68deae244af38b
akram <168238434+akramelassri@users.noreply.github.com>
2025-12-18 18:33:57 +0100

    Add toast structure

---
+++ frontend/toast.js
@@ -0,0 +1,21 @@
+function createSuccessToast(message) {
+
+    const toast = document.createElement('div');
+
+    toast.classList.add('toast-box success-toast-box');
+
+    toast.textContent = message;
+
+    return toast;
+}
+
+function createErrorToast() {
+    const toast = document.createElement('div');
+
+    toast.classList.add('toast-box error-toast-box');
+
+    toast.textContent = message;
+
+    return toast;
+}
+
