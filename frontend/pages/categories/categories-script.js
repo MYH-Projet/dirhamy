15c3c2b190181fdac18080a777ff30a89b563f98
akram <168238434+akramelassri@users.noreply.github.com>
2026-01-01 15:29:12 +0100

    Design and integrate list categories

--- frontend/pages/categories/categories-script.js
+++ frontend/pages/categories/categories-script.js
@@ -1,8 +1,36 @@
-import { loadInitialStructure } from "../../utils/utils.js";
+import {
+  loadInitialStructure,
+  deleteIcon,
+  editIcon,
+  API_URL,
+} from "../../utils/utils.js";
 
 const user = {};
 
 // Create Initial Structure and populate the user object
 loadInitialStructure(user).then(() => {
   // write your code here
+  getCategories();
 });
+
+function getCategories() {
+  return fetch(API_URL + "/categories")
+    .then((res) => res.json())
+    .then((data) => renderCategories(data));
+}
+
+function renderCategories(categories) {
+  const tableBody = document.querySelector("table.list-entity-container tbody");
+  categories.forEach((category) => {
+    const tableRow = document.createElement("tr");
+    const categoryName = document.createElement("td");
+    categoryName.textContent = category.nom;
+
+    const actionBtnsRow = document.createElement("td");
+    actionBtnsRow.classList.add("action-btns");
+    actionBtnsRow.innerHTML = editIcon + deleteIcon;
+
+    tableRow.append(categoryName, actionBtnsRow);
+    tableBody.appendChild(tableRow);
+  });
+}
--- frontend/utils/utils.js
+++ frontend/utils/utils.js
@@ -3,7 +3,7 @@ import {
   disableCurrentSidebarLink,
 } from "../components/sidebar.js";
 
-const API_URL = "/api";
+export const API_URL = "/api";
 
 function renderInitialStucture(user) {
   return insertSidebar(document.querySelector("body")).then(() => {
@@ -26,3 +26,14 @@ export function loadInitialStructure(user) {
       return renderInitialStucture(user);
     });
 }
+
+export const editIcon = `<svg class="edit-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" stroke-linecap="round" stroke-linejoin="round" />
+</svg>`;
+
+export const deleteIcon = `<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+  <path d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" stroke-linecap="round" stroke-linejoin="round" />
+  <line x1="10" y1="11" x2="10" y2="17" stroke-linecap="round" />
+  <line x1="14" y1="11" x2="14" y2="17" stroke-linecap="round" />
+</svg>`;
