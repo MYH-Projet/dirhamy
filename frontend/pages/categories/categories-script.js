e481965de1e3fc24c4830b1c9aae8fcaec62e151
akram <168238434+akramelassri@users.noreply.github.com>
2026-01-13 12:07:33 +0100

    Implement delete category logic

--- frontend/pages/categories/categories-script.js
+++ frontend/pages/categories/categories-script.js
@@ -22,6 +22,9 @@ function getCategories() {
 
 function renderCategories(categories) {
   const tableBody = document.querySelector("table.list-entity-container tbody");
+
+  tableBody.innerHTML = "";
+
   categories.forEach((category) => {
     const tableRow = document.createElement("tr");
     const categoryName = document.createElement("td");
@@ -90,7 +93,7 @@ function showEditCategoryModal(category) {
 
   /*
   This is good if we have a lot of fields i kept it here for future reference but now its just overhead
-  you just pass in fields instead of just nameField
+  you use fields object if we have a lot to manage
   const fields = {
     nom : editIcon.querySelector("#category-name-field"),
   }
@@ -105,34 +108,35 @@ function showEditCategoryModal(category) {
   editModal.addEventListener("submit", (e) => {
     e.preventDefault();
 
+    modalBackground.style.display = "none";
+    editModal.style.display = "none";
+
     if (e.submitter.classList.contains("action-btn")) {
-      submitEditCategoryModel(category, modalBackground, editModal, nameField);
+      const newCategoryFields = {
+        nom: nameField.value,
+      };
+      submitEditCategory(category, newCategoryFields);
     } else if (e.submitter.classList.contains("cancel-btn")) {
-      cancelEditCategoryModel(category, modalBackground, editModal, nameField);
+      // remove the event listener, to try using removeEventListener later
+      editModal.parentElement.replaceChild(
+        editModal.cloneNode(true),
+        editModal
+      );
     }
+
+    nameField.value = "";
   });
 }
 
-function submitEditCategoryModel(
-  category,
-  modalBackground,
-  editModal,
-  nameField
-) {
-  return fetch(API_URL + "/categories" + "/" + category.id, {
+function submitEditCategory(category, newCategoryFields) {
+  return fetch(API_URL + "/categories/" + category.id, {
     method: "PUT",
     headers: {
       "Content-type": "application/json",
     },
-    body: JSON.stringify({
-      nom: nameField.value,
-    }),
+    body: JSON.stringify(newCategoryFields),
   }).then((res) =>
     res.json().then((data) => {
-      modalBackground.style.display = "none";
-      editModal.style.display = "none";
-      nameField.value = "";
-
       if (res.ok) {
         return getCategories().then(() => {
           displayToast(
@@ -144,6 +148,7 @@ function submitEditCategoryModel(
       } else {
         displayToast(
           document.querySelector(".toasts-container"),
+          /* right now i want to see the backend message of error if its not good change it to a normal default message*/
           data.message || data.error,
           "error"
         );
@@ -151,23 +156,55 @@ function submitEditCategoryModel(
     })
   );
 }
-function cancelEditCategoryModel(
-  category,
-  modalBackground,
-  editModal,
-  nameField
-) {
-  modalBackground.style.display = "none";
-  editModal.style.display = "none";
-  nameField.value = "";
 
-  // This is to remove the previous event listeners, there is another way to try later .removeEventListener
-  editModal.parentElement.replaceChild(editModal.cloneNode(true), editModal);
-}
 function showDeleteCategoryModal(category) {
   const modalBackground = document.querySelector(".modal-background");
   const deleteModal = document.querySelector(".delete-category-modal");
   modalBackground.style.display = "block";
   deleteModal.style.display = "block";
+
+  deleteModal.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    modalBackground.style.display = "none";
+    deleteModal.style.display = "none";
+
+    if (e.submitter.classList.contains("delete-btn")) {
+      submitDeleteCategory(category);
+    } else if (e.submitter.classList.contains("cancel-btn")) {
+      // Again removing the eventlistener
+      editModal.parentElement.replaceChild(
+        deleteModal.cloneNode(true),
+        deleteModal
+      );
+    }
+  });
 }
-function removeDeleteCategoryModal(category) {}
+
+function submitDeleteCategory(category) {
+  return fetch(API_URL + "/categories/" + category.id, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) =>
+    res.json().then((data) => {
+      if (res.ok) {
+        return getCategories().then(() =>
+          displayToast(
+            document.querySelector(".toasts-container"),
+            "Success deleting that category",
+            "success"
+          )
+        );
+      } else {
+        displayToast(
+          document.querySelector(".toasts-container"),
+          /* right now i want to see the backend message of error if its not good change it to a normal default message*/
+          data.error || data.message,
+          "error"
+        );
+      }
+    })
+  );
+}
