7d3c33c25a82c7419af0fdb44166ebc607f630e3
anasMAC <anasgamer800@gmail.com>
2026-01-21 12:37:13 +0100

    feat: add the schema of new endpoint

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { register, login, logout,resetPassword,forgetpassword,checkCode } from '../controllers/authController.js';
 import { validation } from '../Middleware/validateResource.js';
-import { loginSchema,registerSchema } from '../schemas/authSchema.js';
+import { loginSchema,registerSchema,resetPasswordSchema,forgetPasswordSchema,checkCodeSchema } from '../schemas/authSchema.js';
 import {authenticateResetpasswordToken} from '../Middleware/authMiddleware.js'
 
 
@@ -11,8 +11,8 @@ router.post('/register',validation(registerSchema), register);
 router.post('/login',validation(loginSchema), login);
 router.post('/logout', logout);
 
-router.post('/reset-password', authenticateResetpasswordToken, resetPassword);
-router.post('/forgetpassword',forgetpassword);
-router.post('/checkCode',checkCode)
+router.post('/reset-password', authenticateResetpasswordToken,validation(resetPasswordSchema), resetPassword);
+router.post('/forgetpassword',validation(forgetPasswordSchema),forgetpassword);
+router.post('/checkCode',validation(checkCodeSchema),checkCode)
 
 export default router;
